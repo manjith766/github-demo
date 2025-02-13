a2678d27f886af507794a4765028957f6c4329ca
manjith chowdary <manjith9989@gmail.com>
2025-02-13 12:15:49 +0530

    emp

--- src/main/java/com/neoteric/HelloWorld.java
+++ src/main/java/com/neoteric/HelloWorld.java
@@ -1,7 +1,18 @@
 package com.neoteric;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
+    public static void main(String[]args) {
+        //student data
+        String studentName = "Arun";
+        int studentId = 16;
+        int studentAge = 24;
+        float studentFee = 85.5f;
+        char studentGrade = 'B';
+        // Print variables
+        System.out.println("studentName: " + studentName);
+        System.out.println("studentId: " + studentId);
+        System.out.println("studentAge " + studentAge);
+        System.out.println("studentFee " + studentFee);
+        System.out.println("studentGrade " + studentGrade);
     }
 }
--- src/main/java/com/neoteric/employee.java
+++ src/main/java/com/neoteric/employee.java
@@ -1,4 +1,17 @@
 package com.neoteric;
 
-public class main {
+public class employee {
+    public static void main(String args[])
+    {
+        String employeName= "arun";
+        int employeId= 24;
+        int employeAge= 25;
+        int employeSalary= 2500;
+        char employeDept= 'A';
+        System.out.println("employeName: "+employeName);
+        System.out.println("employeId: "+employeId);
+        System.out.println("employeAge: "+employeAge);
+        System.out.println("employeSalary: "+employeSalary);
+        System.out.println("employeDept: "+employeDept);
+    }
 }
